f6ae6d0e657cd7a0f9f52edf20e191c88e05b563
shahazad08 <sk.shahazad@gmail.com>
2021-09-09 17:03:28 +0530

    UC2 file added

--- snakeandladder.java
+++ snakeandladder.java
@@ -2,5 +2,8 @@ class SnakeAndLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome TO Snake And Ladder Program");
 		int position=0;
-}
+   int dieRoll=1+(int)Math.floor(Math.random()*10)%6;
+            System.out.println("Die Rolled:"+dieRoll);
+        }
+
 }
